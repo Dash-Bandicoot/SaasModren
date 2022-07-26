b0093633e54b76eb35907983ce668b96c1602931
Arslan <anwaranees250@gmail.com>
2022-07-26 16:53:44 +0500

    Fix: Add landing page

--- packages/landing/src/pages/index.js
+++ packages/landing/src/pages/index.js
@@ -1,66 +1,110 @@
-import React, { Fragment } from 'react';
-import Head from 'next/head';
-import Sticky from 'react-stickynode';
-import { ThemeProvider } from 'styled-components';
-import { agencyTheme } from 'common/theme/agency';
-import ResetCSS from 'common/assets/css/style';
-import { GlobalStyle, AgencyWrapper } from 'containers/Agency/agency.style';
-import Navbar from 'containers/Agency/Navbar';
-import BannerSection from 'containers/Agency/BannerSection';
-import FeatureSection from 'containers/Agency/FeatureSection';
-import AboutUsSection from 'containers/Agency/AboutUsSection';
-import WorkHistory from 'containers/Agency/WorkHistory';
-import BlogSection from 'containers/Agency/BlogSection';
-import TestimonialSection from 'containers/Agency/TestimonialSection';
-import TeamSection from 'containers/Agency/TeamSection';
-import VideoSection from 'containers/Agency/VideoSection';
-import NewsletterSection from 'containers/Agency/NewsletterSection';
-import QualitySection from 'containers/Agency/QualitySection';
-import Footer from 'containers/Agency/Footer';
-import { DrawerProvider } from 'common/contexts/DrawerContext';
-import FaqSection from 'containers/Agency/FaqSection';
+import React, { Fragment, useEffect, useState } from "react";
+import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+import Sticky from "react-stickynode";
+import { DrawerProvider } from "common/contexts/DrawerContext";
+import { saasModernTheme } from "common/theme/saasModern";
+import Loader from '../common/components/Loader';
+import ResetCSS from "common/assets/css/style";
+import {
+  GlobalStyle,
+  ContentWrapper,
+} from "containers/SaasModern/sassModern.style";
 
-const Main = () => {
-  return (
-    <ThemeProvider theme={agencyTheme}>
+import BannerSection from "containers/SaasModern/Banner";
+import Navbar from "containers/SaasModern/Navbar";
+import WorkingProcessSection from "containers/SaasModern/WorkingProcess";
+import PricingSection from "containers/SaasModern/Pricing";
+import PartnerSection from "containers/SaasModern/Partner";
+import FaqSection from "containers/SaasModern/Faq";
+import TrialSection from "containers/SaasModern/Trial";
+import InfoSection from "containers/SaasModern/Info";
+import FeatureSection from "containers/SaasModern/Feature";
+import UpdateScreen from "containers/SaasModern/UpdateScreen";
+import TestimonialSection from "containers/SaasModern/Testimonial";
+import getPage from "../common/components/ContentFull/contentFull";
+import Footer from "containers/SaasModern/Footer";
+
+const SaasModern = () => {
+  const [data, setData] = useState(null);
+  const generateData = (items) => {
+    return Object.assign(
+      ...items.map((entity) => ({ [entity.fields.uid]: entity.fields }))
+    );
+  };
+
+  useEffect(async () => {
+    const params = {
+      content_type: "saasModern",
+    };
+    let client = getPage();
+    let result = await client.getEntries(params);
+    const mappedData = generateData(result?.items);
+    setData(mappedData);
+
+    //   setData(result.items)
+  }, []);
+  console.log(data);
+
+  return !data ? (
+    <div style={{display:"flex",justifyContent:"center", alignItems:"center",height:"100vh"}}>
+      <Loader loaderColor={ "#30C56D"} width="100px" height="100px"/>
+    </div>
+  ) : (
+    <ThemeProvider theme={saasModernTheme}>
       <Fragment>
-        {/* Start agency head section */}
         <Head>
-          <title>Agency | A react next landing page</title>
-          <meta name="theme-color" content="#10ac84" />
+          <title>SaaS | A react next landing page</title>
           <meta name="Description" content="React next landing page" />
+          <meta name="theme-color" content="#ec5555" />
           {/* Load google fonts */}
           <link
-            href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,400i,500,500i,700,900|Open+Sans:400,400i,600,700"
             rel="stylesheet"
           />
         </Head>
+
         <ResetCSS />
         <GlobalStyle />
-        {/* End of agency head section */}
-        {/* Start agency wrapper section */}
-        <AgencyWrapper>
+
+        <ContentWrapper>
           <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
             <DrawerProvider>
               <Navbar />
             </DrawerProvider>
           </Sticky>
-          <BannerSection />
-          <FeatureSection />
-          <AboutUsSection />
-          <WorkHistory />
-          <BlogSection />
-          <QualitySection />
-          <VideoSection />
-          <TestimonialSection />
-          <TeamSection />
-          <FaqSection />
-          <NewsletterSection />
-          <Footer />
-        </AgencyWrapper>
-        {/* End of agency wrapper section */}
+          {data && (
+            <>
+              {console.log(data["platform"])}
+              <BannerSection
+                bannerContent={data["platform"]}
+                contactUsContent={data["contact"]}
+                videoContent={data["video"]}
+              />
+              <WorkingProcessSection
+                workingProcessContent={data["working_process"]}
+              />
+              <InfoSection
+                infoSectionContent={data["visitors"]}
+                howWorkContent={data["works"]}
+              />
+              <FeatureSection featureContent={data["features"]} />
+              <UpdateScreen updateContent={data["update"]} />
+              <PricingSection pricingContent={data["pricing"]} />
+              <PartnerSection partnerContent={data["business"]} />
+              <TestimonialSection />
+              <FaqSection faqContent={data["questions"]} />
+              <TrialSection />
+              <Footer
+                securityContent={data["security"]}
+                aboutContent={data["about"]}
+                privacyContent={data["privacy"]}
+              />
+            </>
+          )}
+        </ContentWrapper>
       </Fragment>
     </ThemeProvider>
   );
 };
-export default Main;
+export default SaasModern;
